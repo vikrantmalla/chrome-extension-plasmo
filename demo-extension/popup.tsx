62df008556a4e9234037cfe7c398b47fd4fae376
bikrant malla <bikrant.malla@truenary.com>
2023-07-20 20:50:39 +0545

    feat: add a tasks in localStorage

--- demo-extension/popup.tsx
+++ demo-extension/popup.tsx
@@ -1,49 +1,59 @@
-import React, { useState, type ChangeEvent, type FormEvent } from "react";
+import React, { useEffect, useState } from "react"
 
 function IndexPopup() {
-  const [tasks, setTasks] = useState<string[]>([]);
-  const [inputValue, setInputValue] = useState<string>('');
+  const [tasks, setTasks] = useState<string[]>([])
+  const [inputValue, setInputValue] = useState<string>("")
 
-  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setInputValue(event.target.value);
-  };
+  // Load tasks from localStorage on component mount
+  useEffect(() => {
+    const storedTasks = JSON.parse(localStorage.getItem("tasks") || "[]")
+    setTasks(storedTasks)
+  }, [])
 
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    if (inputValue.trim() !== '') {
-      setTasks([...tasks, inputValue.trim()]);
-      setInputValue('');
+  useEffect(() => {
+    // Save tasks to localStorage whenever tasks state changes
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+  }, [tasks])
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value)
+  }
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    if (inputValue.trim() !== "") {
+      setTasks([...tasks, inputValue.trim()])
+      setInputValue("")
     }
-  };
+  }
 
   const handleTaskRemoval = (index: number) => {
-    const updatedTasks = [...tasks];
-    updatedTasks.splice(index, 1);
-    setTasks(updatedTasks);
-  };
+    const updatedTasks = [...tasks]
+    updatedTasks.splice(index, 1)
+    setTasks(updatedTasks)
+  }
 
   return (
     <div className="App">
-    <h1>Todo Listdfgfdg</h1>
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        value={inputValue}
-        onChange={handleInputChange}
-        placeholder="Enter a new task..."
-      />
-      <button type="submit">Add Task</button>
-    </form>
-    <ul>
-      {tasks.map((task, index) => (
-        <li key={index}>
-          {task}
-          <button onClick={() => handleTaskRemoval(index)}>Remove</button>
-        </li>
-      ))}
-    </ul>
-  </div>
-
+      <h1>Todo List</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={handleInputChange}
+          placeholder="Enter a new task..."
+        />
+        <button type="submit">Add Task</button>
+      </form>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task}
+            <button onClick={() => handleTaskRemoval(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
